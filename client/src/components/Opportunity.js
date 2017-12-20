e118c60e053d36fa31c19aba29f7392b5fa700c5
Alex McLeod <AlexMcLeod01@gmail.com>
2017-12-19 20:49:43 -0600

    added concurrently to package.json, added link to profile from opportunity page

--- client/src/components/Opportunity.js
+++ client/src/components/Opportunity.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Example from '../img/download.jpeg'
+import { Link } from 'react-router-dom';
 
 const Opportunity = (props) => {
     const mockData = {
       opportunityId: 1,
       title: 'Lorm Ipsum',
       nonProfit: 'Hotdog Fingers Society',
+      userid: 'hotdogFings',
       startDate: '12/30/2017 12:00:00 AM',
       endDate: '12/31/2017 12:00:00 AM',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quod, officia expedita aliquid vero dolorum, veritatis perferendis illum esse dicta nulla aspernatur earum itaque dolorem id distinctio, voluptatibus voluptatem nesciunt!',
@@ -28,7 +30,7 @@ const Opportunity = (props) => {
           <button>Interested</button>
         </div>
         <div>
-          <h1>{mockData.nonProfit}</h1>
+          <Link to={"/profile/"+mockData.userid}><h1>{mockData.nonProfit}</h1></Link>
           <h2>{`in ${mockData.location.address}`}</h2>
           <div>
             <img src={Example} alt="placeholder"/>
