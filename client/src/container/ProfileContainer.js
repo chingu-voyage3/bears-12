d795263a6c0c6f71300db60569fe1a0ba8610e74
thomas <nealthom@gmail.com>
2018-01-10 18:00:06 -0600

    non profit profile

--- client/src/container/ProfileContainer.js
+++ client/src/container/ProfileContainer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Profile from "../components/Profile.js";
 import CalendarContainer from "../container/CalendarContainer.js";
-
+import NonprofitUserContainer from "./NonprofitUserContainer";
 /**
  * ProfileContainer
  */
 export class ProfileContainer extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const mockData = [{
+    const mockData = {
       name: 'Hotdog Fingers Society',
       userType: 'Non-Profit',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quod, officia expedita aliquid vero dolorum, veritatis perferendis illum esse dicta nulla aspernatur earum itaque dolorem id distinctio, voluptatibus voluptatem nesciunt!',
@@ -17,15 +17,30 @@ export class ProfileContainer extends Component { // eslint-disable-line react/p
         long: 0
       },
       userid: 1
-    }];
-    return (
+    };
+    let profile = null;
+    if( mockData.userType === 'Non-Profit'){
+      profile = 
+      <div>
+          <NonprofitUserContainer />
+          <CalendarContainer/>  
+      </div>
+
+    } else {
+      profile =
       <div>
         {
           mockData.map((data, idx) => {
             return <Profile key={idx} data={data} />
           })
         }
-        <CalendarContainer/>
+         
+      </div>
+    }
+
+    return (
+      <div>
+      {profile}
       </div>
     );
   }
