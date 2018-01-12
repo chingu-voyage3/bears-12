e8f8f22618902c1dd1f64f53c21bf6a69404e0f5
thomas <nealthom@gmail.com>
2018-01-12 05:39:01 -0600

    profiles

---
+++ client/src/components/VolunteerProfile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import image from '../img/original.png'
+
+
+const VolunteerProfile = (props) => {
+  return (
+    <div style={ {border: 'solid 1px black'}}>
+      <h3>{props.data.name}</h3>
+      <img src={image} alt=""/>      
+      <p>{props.data.bio}</p>
+ 
+    </div>
+  )
+}
+
+export default VolunteerProfile;
